61abe15662a6f29a44007721045e67d0352e1d45
Jerry <yury@skulbedau.by>
2023-06-11 06:39:17 +0300

    Homework #6

---
+++ task6.41/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    double number;
+    while (!double.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write("Ivvalid value. Please enter a valid number: ");
+    }
+    return number;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.WriteLine($"[{string.Join(", ", arr)}]");
+}
+
+int CountPositiveNumbers(double[] arr)
+{
+    int count = 0;
+    foreach (var item in arr)
+    {
+        if (item > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int arrayLength = 5;
+double[] arrayOfNumbers = new double[arrayLength];
+Console.WriteLine($"Please enter {arrayLength} numbers: ");
+
+for (int i = 0; i < arrayLength; i++)
+{
+    arrayOfNumbers[i] = Prompt("");
+}
+
+Console.Write($"There are {CountPositiveNumbers(arrayOfNumbers)} positive numbers in array ");
+PrintArray(arrayOfNumbers);
---
+++ task6.43/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    double number;
+    while(!double.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write("Ivvalid value. Please enter a valid number: ");
+    }
+    return number;
+}
+
+void FindIntersection(double b1, double b2, double k1, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"The point of intersection: ({x}, {y})");
+}
+
+bool IsValidate(double k1, double k2)
+{
+    return k1 != k2;
+}
+
+
+Console.WriteLine("For an equation like y = k*x + b, enter the coefficients k and b");
+double b1 = Prompt("Enter b1: ");
+double k1 = Prompt("Enter k1: ");
+double b2 = Prompt("Enter b2: ");
+double k2 = Prompt("Enter k2: ");
+
+if (IsValidate(k1, k2))
+{
+    FindIntersection(b1, b2, k1, k2);    
+}
+else
+{
+    Console.WriteLine("There is no point of intersection");
+}
